f21e97ed7f92a401f0b324021b23aba0a44c26eb
Minh Toàn <92781558+2001phamtoan2005@users.noreply.github.com>
2022-06-14 13:44:32 +0700

    lan dau commit

---
+++ DoAn/Models/Cart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn.Models
+{
+    public class Cart
+    {
+        KFCDataContext db = new KFCDataContext();
+        public string sMasp { get; set; }
+        public string sTensp { get; set; }
+        public string sAnh { get; set; }
+        public int dGia { get; set; }
+        public int iSoLuong { get; set; }
+        public string sDescribe { get; set; }
+        public int dThanhTien {
+            get { return iSoLuong * dGia; }
+        }
+        public Cart(CART giohang)
+        {
+            sMasp = giohang.ID_PRODUCT;
+            sTensp = giohang.NAME_PRODUCT;
+            sAnh = giohang.IMAGE_PRODUCT;
+            dGia = Convert.ToInt32(giohang.COST);
+            sDescribe = giohang.DESCRIBE;
+            iSoLuong = Convert.ToInt32(giohang.QUANTITY_PRODUCT);
+        }
+
+        public Cart(string masp)
+        {
+            sMasp = masp;
+            PRODUCT sp = db.PRODUCTs.Single(t => t.ID_PRODUCT == masp);
+            sTensp = sp.NAME_PRODUCT;
+            sAnh = sp.IMAGE_PRODUCT;
+            sDescribe = sp.DESCRIBE;
+            dGia = Convert.ToInt32(sp.COST);
+            iSoLuong = 1;
+        }
+
+        public Cart(string masp,int sl)
+        {
+            sMasp = masp;
+            PRODUCT sp = db.PRODUCTs.Single(t => t.ID_PRODUCT == masp);
+            sTensp = sp.NAME_PRODUCT;
+            sAnh = sp.IMAGE_PRODUCT;
+            sDescribe = sp.DESCRIBE;
+            dGia = Convert.ToInt32(sp.COST);
+            iSoLuong = sl;
+        }
+
+
+    }
+}
